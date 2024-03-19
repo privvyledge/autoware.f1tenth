3241469e1c41ba0d6bf464f2def586568657dc4d
Boluwatife Olabiran <bso19a@fsu.edu>
2024-03-19 17:23:03 -0400

    Added f1tenth dockerfile. Setup scripts to install autoware dependencies and ros.

--- colcon_build.sh
+++ colcon_build.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+skipped_keys="rf2o_laser_odometry librealsense2 realsense2_camera libopencv-dev"
+rosdep install --from-paths src --ignore-src -r -y --skip-keys "${skipped_keys}"
+colcon build --symlink-install --event-handlers console_direct+ --base-paths src --cmake-args '  -Wno-dev' ' --no-warn-unused-cli' ' -DBUILD_ACCELERATE_GPU_WITH_GLSL=ON' ' -DCMAKE_BUILD_TYPE=Release' ' -DCMAKE_EXPORT_COMPILE_COMMANDS=ON' ' -DCMAKE_LIBRARY_PATH=/usr/local/cuda/lib64/stubs' ' -DCMAKE_CXX_FLAGS="-Wl,--allow-shlib-undefined"  -DDOWNLOAD_ARTIFACTS=ON'
