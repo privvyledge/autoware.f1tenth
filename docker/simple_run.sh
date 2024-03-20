c2f95e9b967aac3f61ba9f65dc879e598ee233e3
Boluwatife Olabiran <bso19a@fsu.edu>
2024-03-20 17:25:44 -0400

    Added cyclone DDS configuration file.

--- docker/simple_run.sh
+++ docker/simple_run.sh
@@ -4,5 +4,7 @@ set -e
 
 #IMAGE="privvyledge/r36.2.0-ros-humble-ml:latest"
 IMAGE="privvyledge/f1tenth:humble-latest"
-docker run -it --rm --runtime nvidia --network host --privileged --volume /dev:/dev --volume ~/shared_dir:/shared_dir \
+docker run -it --rm --runtime nvidia --network host --privileged \
+        --volume /dev:/dev --volume ~/shared_dir:/shared_dir \
+        --volume "${HOME}"/cyclone_dds_settings:"${BUILD_HOME}"/src/autoware.f1tenth/cyclone_dds \
         "${IMAGE}"
